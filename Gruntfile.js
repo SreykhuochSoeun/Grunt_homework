a9f72189c3d39f5675b0b857968c046a4c46baad
Sreykhuoch.Soeun <sreykhuoch.soeun@PNC.LAN>
2019-04-06 14:16:18 +0700

    Grunt_homework

---
+++ Gruntfile.js
@@ -0,0 +1,32 @@
+module.exports=function (grunt) {
+    grunt.initConfig({
+        concat:{
+            js:{
+            src:['js/1.js','js/2.js'],
+            dest:'build/js/script.js',
+        },
+        css:{
+            src:['css/min.css','css/theme.css'],
+            dest:'build/css/style.css',
+        },
+    },
+    watch:{
+        js:{
+            files:['js/**/*.js'],
+            tasks:['concat'],
+            
+        },
+        css:{
+            files:['css/**/*.css'],
+            tasks:['concat'],
+            
+        },
+    },
+    });
+
+
+   grunt.loadNpmTasks('grunt-contrib-concat');
+   grunt.loadNpmTasks('grunt-contrib-watch');
+   
+    
+}
